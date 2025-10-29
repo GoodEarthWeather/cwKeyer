4b3db09eb55203b801d08c5586dd9025edf508d5
GoodEarthWeather <fishineer@gmail.com>
2025-10-29 10:43:42 -0700

    Changed location of repo to be outside of CCS workspace

--- Debug/init.d
+++ Debug/init.d
@@ -2,88 +2,143 @@
 
 init.obj: ../init.c
 init.obj: ../main.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/driverlib.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
-init.obj: C:/ti/ccs1011/ccs/ccs_base/msp430/include/msp430.h
-init.obj: C:/ti/ccs1011/ccs/ccs_base/msp430/include/msp430fr2422.h
-init.obj: C:/ti/ccs1011/ccs/ccs_base/msp430/include/in430.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics_legacy_undefs.h
-init.obj: C:/ti/ccs1011/ccs/ccs_base/msp430/include/legacy.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/stdint.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_ti_config.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/linkage.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_stdint40.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/stdint.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/cdefs.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_types.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_types.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_stdint.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_stdint.h
-init.obj: C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/stdbool.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/rtc.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/sysctl.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/crc.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/lcd_e.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/cs.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/ecomp.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/mpy32.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/framctl.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/adc.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/sac.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/sfr.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/wdt_a.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/timer_a.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/pmm.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/gpio.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/tlv.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/tia.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/timer_b.h
-init.obj: C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/driverlib.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h
+init.obj: /home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430.h
+init.obj: /home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr2422.h
+init.obj: /home/david/ti/ccs1281/ccs/ccs_base/msp430/include/in430.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+init.obj: /home/david/ti/ccs1281/ccs/ccs_base/msp430/include/legacy.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+init.obj: /home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/rom_driverlib.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/rom_headers/rom_driverlib_fr253x_fr263x.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/lcd_e.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/ecomp.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/cs.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/sac.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/timer_b.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/framctl.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/tia.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/tlv.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/timer_a.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/pmm.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/sysctl.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/sfr.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/mpy32.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/wdt_a.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/icc.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/gpio.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/adc.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/crc.h
+init.obj: /home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/rtc.h
 
-../init.c: 
-../main.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/driverlib.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h: 
-C:/ti/ccs1011/ccs/ccs_base/msp430/include/msp430.h: 
-C:/ti/ccs1011/ccs/ccs_base/msp430/include/msp430fr2422.h: 
-C:/ti/ccs1011/ccs/ccs_base/msp430/include/in430.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/intrinsics_legacy_undefs.h: 
-C:/ti/ccs1011/ccs/ccs_base/msp430/include/legacy.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/stdint.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_ti_config.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/linkage.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/_stdint40.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/stdint.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/cdefs.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_types.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_types.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/machine/_stdint.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/sys/_stdint.h: 
-C:/ti/ccs1011/ccs/tools/compiler/ti-cgt-msp430_20.2.1.LTS/include/stdbool.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/rtc.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/sysctl.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/crc.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/lcd_e.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/cs.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/ecomp.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/mpy32.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/framctl.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/adc.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/sac.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/sfr.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/wdt_a.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/timer_a.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/pmm.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/gpio.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/tlv.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/tia.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/timer_b.h: 
-C:/Users/fishi/Documents/workspace_v8/keyerModule/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h: 
+../init.c:
+
+../main.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/driverlib.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/inc/hw_memmap.h:
+
+/home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430.h:
+
+/home/david/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr2422.h:
+
+/home/david/ti/ccs1281/ccs/ccs_base/msp430/include/in430.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+/home/david/ti/ccs1281/ccs/ccs_base/msp430/include/legacy.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+/home/david/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/rom_driverlib.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/rom_headers/rom_driverlib_fr253x_fr263x.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/rom_map_driverlib.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/lcd_e.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/eusci_b_i2c.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/ecomp.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/cs.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/sac.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/timer_b.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/framctl.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/eusci_a_spi.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/eusci_a_uart.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/tia.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/tlv.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/timer_a.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/pmm.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/sysctl.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/sfr.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/mpy32.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/wdt_a.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/icc.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/gpio.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/eusci_b_spi.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/adc.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/crc.h:
+
+/home/david/git/cwKeyer/driverlib/MSP430FR2xx_4xx/rtc.h:
+
